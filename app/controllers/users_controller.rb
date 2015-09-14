2dce921e210251b2a1cf286b18f86413029d2975
Juan Cacho <juancacho@ymail.com>
2015-09-14 14:13:02 -0700

    added registration function in create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
   end
 
   def create
+    if User.create(user_params)
+        #redicret to signed in
+        flash[:success] = 'you are registered'
+        redirect_to users_path
+      else
+
+        flash[:error] = 'registration has failed'
+
+        redirect_to new_session_path
+
+      end
   end
 
   def destroy
