8ab70df4d8c502656c6848230e2f396c9f407a81
Juan Cacho <juancacho@ymail.com>
2015-09-14 14:48:13 -0700

    update user_params method in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -34,6 +34,6 @@ class SessionsController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:email, :user_name, :password, :image_url)
+    params.require(:user).permit(:email, :password)
   end
 end
